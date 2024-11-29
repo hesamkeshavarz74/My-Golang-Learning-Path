b6cbf3fffcf38f26139dc657759019df961cfe5b
hesamkeshavarz74 <hesamkeshavarz74@gmail.com>
2024-11-29 18:59:12 +0330

    Definition

---
+++ Module 1 /datatypes.go
@@ -0,0 +1,13 @@
+package main
+
+/*	a data type is a way to specify
+	how data should be interpreted
+	go uses static typing , which is checked at compile time
+	-the compiler uses type interference which automatically
+	detremines which types to use .
+	Type aliases can be created to give new names to exisiting
+	types .
+	type UserID int
+	Converting between types requires parentheses
+	UserID(5)
+*/
